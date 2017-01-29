3ac43e9d9d46e1973e2fd0063bfa200582512ea7
hamishdickson <hamish.dickson@gmail.com>
2017-01-29 17:31:02 +0000

    create sheet

--- src/main/scala/scalapoi/dragons/Sheet.scala
+++ src/main/scala/scalapoi/dragons/Sheet.scala
@@ -2,21 +2,18 @@ package scalapoi
 package dragons
 
 import org.apache.poi.hssf.usermodel._
+import org.apache.poi.ss.util.WorkbookUtil._
 import java.io.{File, FileOutputStream}
 import scala.collection.JavaConverters._
 
 object Sheet {
   // totes unsafe
   def create(name: String, wb: HSSFWorkbook): HSSFSheet = {
-    val sheet = wb.createSheet(name)
-  //  val fileOut = new FileOutputStream("thing.xlsx");
-  //  wb.write(fileOut);
-  //  fileOut.close();
+    val safeName = createSafeSheetName(name)
+    val sheet = wb.createSheet(safeName)
+    val fileOut = new FileOutputStream("things.xlsx");
+    wb.write(fileOut);
+    fileOut.close();
     sheet
-  }
-
-  // todo implement
-  private def validateName(n: String): Either[String, String] = {
-    Right(n)
   }
 }
